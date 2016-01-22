9a196037d3c3d080dedb0eca421ebc51e5218ac0
Taha Rushain <taharushain@gmail.com>
2016-01-22 23:28:06 +0500

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.default_url_options = { host: 'rnrblog.herokuapp.com', port: 49067 }
-
 end
